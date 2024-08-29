c37708a37ee2d37c10249512453c965ec667c65d
Dhruv Patel <dhruv.patel@primedemo.in>
2024-08-29 08:44:56 +0530

    index

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ mongoose.connect(process.env.MONGO_URI, {
   .catch((err) => console.log('Error connecting to MongoDB:', err));
 
 // Routes
-app.use('/api/auth', require('./routes/auth'));
+app.use('/api/auth', require('./src/routes/auth'));
 
 // Start server
 const PORT = process.env.PORT || 5000;
